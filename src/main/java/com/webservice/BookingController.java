7a9adf2ebd3d7bc949b03e666c56876e0b654c29
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2016-08-01 15:47:29 +0100

    Added POST(create)/PUT(update)/DELETE functionality to bookings.

--- src/main/java/com/webservice/BookingController.java
+++ src/main/java/com/webservice/BookingController.java
@@ -1,14 +1,46 @@
 package com.webservice;
 
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookingController {
 
-    @RequestMapping("/booking")
-    public String booking(@RequestParam(value="name", defaultValue="") String name) {
+    @RequestMapping(value = "/booking", method=RequestMethod.GET)
+    public String getAllBookingsForUser(@RequestParam(value="name", defaultValue="") String name) {
      return "hi";
     }
+    
+    //POST - create new booking
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/booking", method=RequestMethod.POST)
+    public @ResponseBody String createBooking(@RequestParam int userID, @RequestParam int deskID, @RequestParam String startDate, @RequestParam String endDate) throws SQLException, ParseException  {
+    	return BookingDAO.createBooking(userID, deskID, startDate, endDate);
+    }
+    
+    //PUT - update existing booking
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/booking/{bookingID}", method=RequestMethod.PUT)
+    public String updateBooking(@PathVariable int bookingID, @RequestParam String newStartDate, @RequestParam String newEndDate) throws SQLException, ParseException  {
+    	return BookingDAO.updateBooking(bookingID, newStartDate, newEndDate);
+    }
+    
+    //DELETE - delete existing booking
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/booking/{bookingID}", method=RequestMethod.DELETE)
+    public @ResponseBody String deleteBooking(@PathVariable int bookingID)   {
+    	return BookingDAO.deleteBooking(bookingID);
+    }
+    
+    
 }
+
+
--- src/main/java/com/webservice/BookingDAO.java
+++ src/main/java/com/webservice/BookingDAO.java
@@ -1,13 +1,21 @@
 package com.webservice;
 
+import java.sql.BatchUpdateException;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Vector;
-
 import com.mysql.jdbc.Connection;
+
 
 public class BookingDAO {
 
@@ -51,17 +59,108 @@ public class BookingDAO {
 		return null;
 	}
 	
+	//Method to create booking for a specified user
+	public static String createBooking(int userID, int deskID, String inputStartDate, String inputEndDate) throws SQLException, ParseException{
+
+		try{
+			//Get connection & disable auto commit for batch excecution
+			Connection conn = BookingDAO.establishConnection();
+			conn.setAutoCommit(false);
+			
+			//Instert into booking table, return auto generated ID
+			PreparedStatement stmt = conn.prepareStatement("INSERT INTO booking (userID_FK, deskID_FK) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, userID);
+			stmt.setInt(2, deskID);
+			stmt.execute();
+			
+			//get auto generated ID for bookingdate table
+			int generatedUserID = 0;
+			try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+	            if (generatedKeys.next()) {
+	                generatedUserID = generatedKeys.getInt(1);
+	            }
+	        }
+			
+			//Insert bookin dates records an commit
+			instertBookingDates(conn, stmt, generatedUserID, inputStartDate, inputEndDate);
+			conn.commit();
+			return "Booking has been created";
+		} catch(SQLException ex) {
+	        return ex.toString();
+	    }
+		
+	}
 	
+	//Method to update booking for a specified user
+	public static String updateBooking(int bookingID, String newStartDate, String newEndDate) throws SQLException, ParseException{
+		try{
+			//Get connection & disable auto commit for batch excecution
+			Connection conn = BookingDAO.establishConnection();
+			conn.setAutoCommit(false);
+			
+			PreparedStatement stmt = conn.prepareStatement("DELETE FROM bookingdate WHERE bookingID_FK = ?");
+			stmt.setInt(1, bookingID);
+			stmt.executeUpdate();
+			
+			//Insert bookin dates records an commit
+			instertBookingDates(conn, stmt, bookingID, newStartDate, newEndDate);
+			conn.commit();
+			return "Booking: " + bookingID + " has been updated.";
+		}catch(SQLException e){
+			return e.toString();
+		}
+		
+	}
+	
+	public static String deleteBooking(int bookingID) {
+		try{
+			//Get connection & disable auto commit for batch excecution
+			Connection conn = BookingDAO.establishConnection();
+			conn.setAutoCommit(false);
+			
+			PreparedStatement stmt = conn.prepareStatement("DELETE FROM booking WHERE bookingID = ?");
+			stmt.setInt(1, bookingID);
+			stmt.executeUpdate();
+			conn.commit();
+			return "Booking " + bookingID + " has been deleted.";
+		}catch(SQLException e){
+			return e.toString();
+		}
+	}
+	
+	
+	private static void instertBookingDates(Connection conn, PreparedStatement stmt, int generatedUserID, String inputStartDate, String inputEndDate) throws SQLException, ParseException{
+		stmt = conn.prepareStatement("INSERT INTO bookingdate (bookingID_FK, date) VALUES(?,?)");
+		LocalDate startDate = LocalDate.parse(inputStartDate);
+		LocalDate endDate = LocalDate.parse(inputEndDate);
+		for(LocalDate ld = startDate; ld.isBefore(endDate.plusDays(1)); ld = ld.plusDays(1)){
+			stmt.setInt(1, generatedUserID);
+			stmt.setDate(2, stringToSQLDate(ld.toString()));
+			stmt.addBatch();
+		 }
+		 stmt.executeBatch();
+	}
+	
+	private static java.sql.Date stringToSQLDate(String stringDate) throws ParseException{
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date dateStr = formatter.parse(stringDate);
+		return  new java.sql.Date(dateStr.getTime());
+	}
 	
 	private static Connection establishConnection(){
 		Connection conn = null;
 		try {
 			// The newInstance() call is a work around for some broken Java implementations
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			conn = (Connection) DriverManager.getConnection("jdbc:mysql://UKL5CG6195GRV:3306/hotdesk_db?" +"user=hotdesk&password=hotdesk");
+			//Use for Michaels DB
+//			conn = (Connection) DriverManager.getConnection("jdbc:mysql://UKL5CG6195GRV:3306/hotdesk_db?" +"user=hotdesk&password=hotdesk");
+			//Use for Reds DB
+			conn = (Connection) DriverManager.getConnection("jdbc:mysql://UKL5CG6195G1Q:3306/hotdesk?" +"user=hotdesk&password=hotdesk");
 		} catch (Exception error) {
 			System.err.println("Could not establish a connection with the DataBase! "+error);
 		}
 		return conn;
 	}
+
+
 }
